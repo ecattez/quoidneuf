81d09ff003dc0809c30b621fc8dea5fd16c22006
Thomas Ferro <t.ferro184@gmail.com>
2016-01-06 11:30:15 +0100

    Début initialisation

---
+++ 02_init.sql
@@ -0,0 +1,29 @@
+-- CATTEZ Edouard - FERRO Thomas
+-- Insert into tables
+
+-- Credential :
+INSERT INTO credential VALUES ('ferrot', 'passsaltferrot', 'passhashferrot');
+INSERT INTO credential VALUES ('catteze', 'passsaltcatteze', 'passhashcatteze');
+INSERT INTO credential VALUES ('leleuj', 'passsaltleleuj', 'passhashleleuj');
+INSERT INTO credential VALUES ('fevrer', 'passsaltfevrer', 'passhashfevrer');
+INSERT INTO credential VALUES ('katerinep', 'passsaltketerinep', 'passhashketrinep');
+INSERT INTO credential VALUES ('vincentf', 'passsaltvincenf', 'passhashvincenf');
+INSERT INTO credential VALUES ('vincentl', 'passsaltvincentl', 'passhashvincentl');
+INSERT INTO credential VALUES ('renk', 'passsaltrenk', 'passhashrenk');
+INSERT INTO credential VALUES ('jacksonsl', 'passsaltjacksonsl', 'passhashjacksonsl');
+INSERT INTO credential VALUES ('scorsesem', 'passsaltscorsesem', 'passhashscorsesem');
+
+-- Subscriber_meta :
+INSERT INTO subscriber_meta VALUES (0, 'picturepath', 'Description blablabla', '0@mail.com', '0000000000');
+INSERT INTO subscriber_meta VALUES (1, 'picturepath', 'Description blablabla', '1@mail.com', '1111111111');
+INSERT INTO subscriber_meta VALUES (2, 'picturepath', 'Description blablabla', '2@mail.com', '2222222222');
+INSERT INTO subscriber_meta VALUES (3, 'picturepath', 'Description blablabla', '3@mail.com', '3333333333');
+INSERT INTO subscriber_meta VALUES (4, 'picturepath', 'Description blablabla', '4@mail.com', '4444444444');
+INSERT INTO subscriber_meta VALUES (5, 'picturepath', 'Description blablabla', '5@mail.com', '5555555555');
+INSERT INTO subscriber_meta VALUES (6, 'picturepath', 'Description blablabla', '6@mail.com', '6666666666');
+INSERT INTO subscriber_meta VALUES (7, 'picturepath', 'Description blablabla', '7@mail.com', '7777777777');
+INSERT INTO subscriber_meta VALUES (8, 'picturepath', 'Description blablabla', '8@mail.com', '8888888888');
+INSERT INTO subscriber_meta VALUES (9, 'picturepath', 'Description blablabla', '9@mail.com', '9999999999');
+INSERT INTO subscriber_meta VALUES (10, 'picturepath', 'Description blablabla', '10@mail.com', '1010101010');
+
+-- Discussion_meta :
