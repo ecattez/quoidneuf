8700de0ae4d5348aa73cf2fc8adce71ca82b3c4e
Thomas Ferro <t.ferro184@gmail.com>
2016-01-18 09:17:12 +0100

    Ajout des "AS"

--- requetes_utiles.sql
+++ requetes_utiles.sql
@@ -32,25 +32,25 @@ SELECT 1 FROM subscriber WHERE birthdate = '19-10-1995';
 
 --- Récupération informations d'un utilisateur :
 
-SELECT login, first_name, last_name, birthdate, picture, description, email, phone FROM subscriber as s INNER JOIN subscriber_meta as sm ON (s.subscriber_id = sm.subscriber_id) WHERE login = 'ferrot';
+SELECT login, first_name, last_name, birthdate, picture, description, email, phone FROM subscriber AS s INNER JOIN subscriber_meta AS sm ON (s.subscriber_id = sm.subscriber_id) WHERE login = 'ferrot';
 
 --- Liste amis d'un utilisateur :
 
-SELECT sub.login FROM subscriber sub WHERE
+SELECT sub.login FROM subscriber AS sub WHERE
   sub.subscriber_id IN (SELECT friend_2 FROM friend_with f INNER JOIN subscriber s ON (subscriber_id = friend_1) WHERE s.login = 'catteze')
   OR sub.subscriber_id IN (SELECT friend_1 FROM friend_with f INNER JOIN subscriber s ON (subscriber_id = friend_2) WHERE s.login = 'catteze');
 
 --- Liste groupes d'un utilisateur :
 
-SELECT discussion_name FROM belong_to b INNER JOIN discussion d ON (d.discussion_id = b.discussion_id) INNER JOIN subscriber s ON (s.subscriber_id = b.subscriber_id) WHERE login = 'fevrer';
+SELECT discussion_name FROM belong_to AS b INNER JOIN discussion AS d ON (d.discussion_id = b.discussion_id) INNER JOIN subscriber AS s ON (s.subscriber_id = b.subscriber_id) WHERE login = 'fevrer';
 
 --- Liste messages d'un utilisateur :
 
-SELECT content, written_date FROM message m INNER JOIN subscriber s ON (s.subscriber_id = m.subscriber_id) WHERE login = 'ferrot' ORDER BY written_date ASC;
+SELECT content, written_date FROM message AS m INNER JOIN subscriber AS s ON (s.subscriber_id = m.subscriber_id) WHERE login = 'ferrot' ORDER BY written_date ASC;
 
 --- Vérifier si l'utilisateur est membre d'un groupe :
 
-SELECT 1 FROM  belong_to b INNER JOIN subscriber s ON (s.subscriber_id = b.subscriber_id) INNER JOIN discussion d ON (d.discussion_id = b.discussion_id)
+SELECT 1 FROM  belong_to AS b INNER JOIN subscriber AS s ON (s.subscriber_id = b.subscriber_id) INNER JOIN discussion AS d ON (d.discussion_id = b.discussion_id)
   WHERE login= 'ferrot' AND discussion_name = 'Nom de la discussion 1';
 
 --- Récupérer rôle :
@@ -61,7 +61,7 @@ SELECT login, role FROM credential WHERE login = 'ferrot';
 
 --- Liste des membres :
 
-SELECT login FROM belong_to b INNER JOIN discussion d ON (d.discussion_id = b.discussion_id) INNER JOIN subscriber s ON (s.subscriber_id = b.subscriber_id)
+SELECT login FROM belong_to AS b INNER JOIN discussion AS d ON (d.discussion_id = b.discussion_id) INNER JOIN subscriber AS s ON (s.subscriber_id = b.subscriber_id)
   WHERE discussion_name = 'Nom de la discussion 1';
 
 --- Vérifier si disponible :
@@ -72,31 +72,31 @@ SELECT enabled FROM discussion WHERE discussion_name = 'Nom de la discussion 1';
 
 ---- Tous les messages :
 
-SELECT subscriber_id as Envoyé_par, m.discussion_id as Envoyé_vers, content as Message, written_date as Date FROM message m
-  INNER JOIN discussion d ON (d.discussion_id = m.discussion_id)
+SELECT subscriber_id AS Envoyé_par, m.discussion_id AS Envoyé_vers, content AS Message, written_date AS Date FROM message AS m
+  INNER JOIN discussion AS d ON (d.discussion_id = m.discussion_id)
   WHERE discussion_name = 'Nom de la discussion 1' ORDER BY written_date ASC;
 
 ---- Par jour :
 
-SELECT subscriber_id as Envoyé_par, m.discussion_id as Envoyé_vers, content as Message, written_date as Date FROM message m
-  INNER JOIN discussion d ON (d.discussion_id = m.discussion_id)
+SELECT subscriber_id AS Envoyé_par, m.discussion_id AS Envoyé_vers, content AS Message, written_date AS Date FROM message AS m
+  INNER JOIN discussion AS d ON (d.discussion_id = m.discussion_id)
   WHERE discussion_name = 'Nom de la discussion 1'
   AND DATE(written_date) = '2016-01-09'
   ORDER BY written_date ASC;
 
 ---- D'un utilisateur donné :
 
-SELECT m.subscriber_id as Envoyé_par, m.discussion_id as Envoyé_vers, content as Message, written_date as Date FROM message m
-  INNER JOIN discussion d ON (d.discussion_id = m.discussion_id)
-  INNER JOIN subscriber s ON (s.subscriber_id = m.subscriber_id)
+SELECT m.subscriber_id AS Envoyé_par, m.discussion_id AS Envoyé_vers, content AS Message, written_date AS Date FROM message AS m
+  INNER JOIN discussion AS d ON (d.discussion_id = m.discussion_id)
+  INNER JOIN subscriber AS s ON (s.subscriber_id = m.subscriber_id)
   WHERE discussion_name = 'Nom de la discussion 1'
   AND login = 'ferrot'
   ORDER BY written_date ASC;
 
 ---- Par contenu :
 
-SELECT subscriber_id as Envoyé_par, m.discussion_id as Envoyé_vers, content as Message, written_date as Date FROM message m
-  INNER JOIN discussion d ON (d.discussion_id = m.discussion_id)
+SELECT subscriber_id AS Envoyé_par, m.discussion_id AS Envoyé_vers, content AS Message, written_date AS Date FROM message AS m
+  INNER JOIN discussion AS d ON (d.discussion_id = m.discussion_id)
   WHERE content LIKE '%1%'
   AND discussion_name = 'Nom de la discussion 1' ORDER BY written_date ASC;
 
@@ -109,7 +109,7 @@ SELECT friend_1, friend_2 FROM friend_with WHERE status = 'f';
 --- Envoyer demande d'ami :
 
 INSERT INTO friend_with (friend_1, friend_2)
-  SELECT s1.subscriber_id, s2.subscriber_id FROM subscriber s1, subscriber s2 WHERE s1.login = 'scorsesem' AND s2.login = 'vincentf';
+  SELECT s1.subscriber_id, s2.subscriber_id FROM subscriber AS s1, subscriber AS s2 WHERE s1.login = 'scorsesem' AND s2.login = 'vincentf';
 
 -- Accepter demande ami :
 
