e173028278382aa5d1a625493a924c3e2336f992
Edouard CATTEZ <edouard.cattez@sfr.fr>
2016-01-27 20:03:05 +0100

    Preparation systeme d'upload de fichiers

--- src/quoidneuf/dao/Dao.java
+++ src/quoidneuf/dao/Dao.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of quoidneuf.
+ *
+ * quoidneuf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * quoidneuf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.				 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with quoidneuf.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Edouard CATTEZ <edouard.cattez@sfr.fr> (La 7 Production)
+ */
 package quoidneuf.dao;
 
 import java.io.Serializable;
@@ -9,6 +27,12 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+/**
+ * Système de Dao simplifié.
+ * 
+ * @param	<T>
+ * 			le type de la clé d'intégrité référentielle
+ */
 public abstract class Dao<T extends Serializable> {
 	
 	/**
